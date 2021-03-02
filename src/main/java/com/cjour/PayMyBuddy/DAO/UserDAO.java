6de1d528593558c2f2776fdcd834e62b29412edf
Clément <jour.clement@gmail.com>
2021-03-02 10:27:23 +0100

    WIP

--- src/main/java/com/cjour/PayMyBuddy/DAO/UserDAO.java
+++ src/main/java/com/cjour/PayMyBuddy/DAO/UserDAO.java
@@ -1,14 +1,13 @@
 package com.cjour.PayMyBuddy.DAO;
 
-import java.util.Collection;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cjour.PayMyBuddy.entity.User;
+import com.cjour.PayMyBuddy.entity.*;
 
 @Repository
 public interface UserDAO extends CrudRepository<User, Integer>{
 
-	Collection<User> findAllById(Integer id);
+	Iterable<User> findAll();
 	
 }
