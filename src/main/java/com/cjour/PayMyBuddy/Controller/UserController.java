d719b7f414386e4d57015317f27db350430b64e4
Clément <jour.clement@gmail.com>
2021-03-02 15:49:19 +0100

    Logging basic system

--- src/main/java/com/cjour/PayMyBuddy/Controller/UserController.java
+++ src/main/java/com/cjour/PayMyBuddy/Controller/UserController.java
@@ -1,13 +1,46 @@
 package com.cjour.PayMyBuddy.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.cjour.PayMyBuddy.DAO.UserDAO;
+import com.cjour.PayMyBuddy.entity.User;
+
+@Controller
 public class UserController {
-	@RequestMapping(value="/Users", method = RequestMethod.GET)
-	public String listeUser() {
-		return "Un exemple de User";		
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	
+	@RequestMapping("/login")
+	public String displayLoginForm() {
+		return "login";
+	}
+	
+	@RequestMapping("/home")
+	public String displayDashboard() {
+		return "home";
+	}
+	
+	@GetMapping(value="Users")
+	public Iterable<User> listAll(){
+		return userDAO.findAll();		
+	}
+	
+	@GetMapping(value="/Users/userName/{userName}")
+	public Iterable<User> listUserByUserName(@PathVariable String userName, Model model) {
+		model.addAttribute("users", userDAO.findByUserName(userName));
+		return null;
+	}
+	
+	@GetMapping(value="/Users/id/{id}")
+	public Optional<User> listUserById(@PathVariable Integer id) {
+		return userDAO.findById(id);		
 	}
 }
