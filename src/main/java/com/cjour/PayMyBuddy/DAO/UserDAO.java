de1d4348c618747044ba13754c4948fd0545297a
Clément <jour.clement@gmail.com>
2021-03-02 15:48:54 +0100

    Creation from database of entity successful
    wip

--- src/main/java/com/cjour/PayMyBuddy/DAO/UserDAO.java
+++ src/main/java/com/cjour/PayMyBuddy/DAO/UserDAO.java
@@ -1,5 +1,7 @@
 package com.cjour.PayMyBuddy.DAO;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.cjour.PayMyBuddy.entity.*;
 @Repository
 public interface UserDAO extends CrudRepository<User, Integer>{
 
-	Iterable<User> findAll();
+	Iterable<User> findByUserName(String userName);
+	Optional<User> findById(Integer id);
 	
 }
--- src/main/java/com/cjour/PayMyBuddy/entity/User.java
+++ src/main/java/com/cjour/PayMyBuddy/entity/User.java
@@ -27,12 +27,36 @@ public class User {
 		super();
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getUserName() {
 		return userName;
 	}
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
 	}
 
 }
